240f559a0d801d4d9df57cd56de4ca6f97d5e0b8
Alejandro <alejandro.morales.santacruz@correounivalle.edu.co>
2023-06-30 23:07:00 -0500

    configuramos

--- Untitled-1.py
+++ Untitled-1.py
@@ -24,8 +24,8 @@ def numeros_amigos(número1, número2):
         return f"{número1} y {número2} no son amigos"
 
 def main():
-    número1 = 220
-    número2 = 284
+    número1 = 200
+    número2 = 250
     resultado = numeros_amigos(número1, número2)
     print(resultado)
 
